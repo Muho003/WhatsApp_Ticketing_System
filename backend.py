16e2ba74c9e8b5802a1512267dbe463f648a5d1c
Muhotasim Ahmed <muhotasim@hotmail.com>
2025-01-12 14:16:56 -0500

    Basic HTML rendering

--- backend.py
+++ backend.py
@@ -1,26 +1,13 @@
 # This is from John Hammond's tutorial
 # Needs to be cleaned up and updated further
 
-from flask import Flask
-from flask import render_template, send_file, make_response, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/auth", methods=["POST"])
-def auth():
-    print(request.form.to_dict())
-    return "ok"
-
 @app.route("/")
-def index():
-    response = make_response(send_file("templates/login.html"))
-    # Just for localhost demonstration sake
-    response.headers.add("Acces-Control-Allow-Origin", "*")
-    return response 
-
-@app.route("/login")
-def login():
-    return send_file("templates/2fa.html")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
